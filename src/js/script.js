f0fc5e2e436c89e0879900c6ddf4d0416447e078
Andrey <andrej.vershinkin@yandex.ru>
2022-04-22 15:44:16 +0300

    full project no responsive

--- src/js/script.js
+++ src/js/script.js
@@ -83,9 +83,43 @@ $(document).ready(function () {
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
-    // $("#phone").mask("(999) 999-9999");
+    $('form').submit(function (e) {
+        e.preventDefault();
 
-    // consultation - form
-    // consultation
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    // smooth scroll and pageup
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1100) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
+
+    new WOW().init();
 });
 
